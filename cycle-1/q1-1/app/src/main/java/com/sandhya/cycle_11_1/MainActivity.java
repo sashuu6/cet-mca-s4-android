7ae4e7d5d864f795831d98a2636f3a1bc57c7342
Sashwat K <sashwat0001@gmail.com>
2019-02-04 12:25:25 +0330

    working 1.1

--- cycle-1/q1-1/app/src/main/java/com/sandhya/cycle_11_1/MainActivity.java
+++ cycle-1/q1-1/app/src/main/java/com/sandhya/cycle_11_1/MainActivity.java
@@ -1,13 +1,27 @@
 package com.sandhya.cycle_11_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button b;
+Intent i;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        b=(Button ) findViewById(R.id.b1);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(i);
+            }
+
+        });
     }
 }
