220609eb4bd4bf05b89071ea9dca92417b8e9aa2
Sashwat K <sashwat0001@gmail.com>
2019-02-13 11:34:09 +0330

    opening browser using button done

---
+++ cycle-1/q4/app/src/main/java/com/sandhya/sashwat/q4/MainActivity.java
@@ -0,0 +1,27 @@
+package com.sandhya.sashwat.q4;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button urlbutton = (Button)findViewById(R.id.button_1);
+        urlbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(
+                        Intent.ACTION_VIEW,
+                        Uri.parse("https:sashwat.in"));
+                startActivity(browserIntent);
+            }
+        });
+    }
+}
