a84dca58ac02ca3c59bd7198686b89611e8f54a9
Sashwat K <sashwat0001@gmail.com>
2019-03-06 13:00:15 +0330

    New code to work

---
+++ cycle-2/DigitalClock/app/src/main/java/com/sandhya/sashwat/myapplication/digitalclock/MainActivity.java
@@ -0,0 +1,65 @@
+package com.sandhya.sashwat.myapplication.digitalclock;
+
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DigitalClock;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextToSpeech textToSpeech;
+    Button speakTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button speakTime= (Button) findViewById(R.id.timeTTS);
+        final DigitalClock simpleDigitalClock = (DigitalClock) findViewById(R.id.clockTime);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textToSpeech.setLanguage(Locale.US);
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        simpleDigitalClock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Digital Clock", Toast.LENGTH_SHORT).show(); //display a toast for digital clock
+            }
+        });
+
+        speakTime.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                String data = simpleDigitalClock.getText().toString();
+                Log.i("TTS", "button clicked: " + data);
+                int speechStatus = textToSpeech.speak(data, TextToSpeech.QUEUE_FLUSH, null);
+
+                if (speechStatus == TextToSpeech.ERROR) {
+                    Log.e("TTS", "Error in converting Text to Speech!");
+                }
+            }
+
+        });
+    }
+}
---
+++ cycle-2/GPSlocation/app/src/main/java/com/example/andlabb/gpslocation/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.andlabb.gpslocation;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
+    Context mContext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        mContext=this;
+        locationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+    }
+    LocationListener location=new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Toast.makeText(getApplicationContext(),location.getLatitude()+","+location.getLongitude() ,Toast .LENGTH_LONG).show();
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(getApplicationContext(),"no permission" ,Toast .LENGTH_LONG).show();
+                        return;
+                    }
+                    else
+                    {
+                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                10,
+                                10, location);
+                    }
+
+
+                } else {
+
+                }
+                return;
+            }
+
+        }
+    }
+}
